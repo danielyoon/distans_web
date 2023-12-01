b6e729351580c3fc93788a2922b1e37507cd587c
Daniel Yoon <danielyoonx3@gmail.com>
2023-12-01 01:49:17 -0500

    Added map controller

---
+++ controllers/map_controller.js
@@ -0,0 +1,6 @@
+var express = require("express"),
+  router = express.Router(),
+  authorize = require("../config/authorize"),
+  userService = require("../services/map_service");
+
+module.exports = router;
