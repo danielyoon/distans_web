138901bc7095c385aa5f175e2f781e4cbe50bfe1
Daniel Yoon <danielyoonx3@gmail.com>
2023-12-02 10:23:07 -0500

    Added logging to scheduler

--- config/scheduler.js
+++ config/scheduler.js
@@ -3,47 +3,53 @@ var db = require("../components/mongo.js");
 module.exports = scheduler;
 
 async function scheduler() {
-  const currentTime = new Date();
+  try {
+    const currentTime = new Date();
 
-  const places = await db.Place.find({}, "users").populate(
-    "users",
-    "time isLocationAlwaysOn"
-  );
+    const places = await db.Place.find({}, "users").populate(
+      "users",
+      "time isLocationAlwaysOn"
+    );
 
-  let usersToUpdate = [];
-  let markersToUpdate = [];
+    let usersToUpdate = [];
+    let markersToUpdate = [];
 
-  for (const place of places) {
-    for (const user of place.users) {
-      const minutesSinceCheckedIn = Math.floor(
-        Math.abs(currentTime - user.time) / 1000 / 60
-      );
+    for (const place of places) {
+      for (const user of place.users) {
+        const minutesSinceCheckedIn = Math.floor(
+          Math.abs(currentTime - user.time) / 1000 / 60
+        );
 
-      let checkoutTime = user.isLocationAlwaysOn ? 360 : 240;
+        let checkoutTime = user.isLocationAlwaysOn ? 360 : 240;
 
-      if (minutesSinceCheckedIn > checkoutTime) {
-        usersToUpdate.push(user.user);
-        markersToUpdate.push(place._id);
+        if (minutesSinceCheckedIn > checkoutTime) {
+          usersToUpdate.push(user.user);
+          markersToUpdate.push(place._id);
+        }
       }
     }
-  }
 
-  if (usersToUpdate.length > 0) {
-    await db.User.updateMany(
-      { _id: { $in: usersToUpdate } },
-      {
-        $set: {
-          currentLocation: "",
-          recentCheckedIn: new Date(0),
-        },
-      }
-    );
+    if (usersToUpdate.length > 0) {
+      await db.User.updateMany(
+        { _id: { $in: usersToUpdate } },
+        {
+          $set: {
+            currentLocation: "",
+            recentCheckedIn: new Date(0),
+          },
+        }
+      );
 
-    await db.Marker.updateMany(
-      { _id: { $in: markersToUpdate } },
-      {
-        $pull: { users: { user: { $in: usersToUpdate } } },
-      }
-    );
+      await db.Marker.updateMany(
+        { _id: { $in: markersToUpdate } },
+        {
+          $pull: { users: { user: { $in: usersToUpdate } } },
+        }
+      );
+    }
+
+    console.log("Scheduler ran successfully.");
+  } catch (error) {
+    console.error("Error in scheduler function:", error);
   }
 }
