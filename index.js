09c8a5a0750e3b818063e95ddadb47cc1ea2a8d4
Daniel Yoon <danielyoonx3@gmail.com>
2024-01-21 04:55:23 -0500

    Added CSP

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ app.use(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; connect-src 'self' https://distans.app;"
+  );
+  next();
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function (req, res) {
