43b0e14854924cc97d4ec2a4608ce0dfd1d4bfa0
Daniel Yoon <danielyoonx3@gmail.com>
2024-01-21 04:39:08 -0500

    Added new build folder

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -42,8 +42,6 @@ function loginWithEmail(req, res, next) {
 function refreshToken(req, res, next) {
   const token = req.cookies.refreshToken;
 
-  console.log(token);
-
   if (!token) {
     return res.sendStatus(404);
   }
