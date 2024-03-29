dbabe25c02cd903f1955e1d1436a4456f3d6da2d
Daniel Yoon <danielyoonx3@gmail.com>
2024-03-29 19:25:52 -0400

    Added new pages

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5000",
       "https://www.distans.app",
       "https://admin.distans.app",
     ],
