177ac95425e76d9a4e66f030b97a72daeb4be813
Daniel Yoon <danielyoonx3@gmail.com>
2024-06-26 23:02:30 -0400

    Added ETA reset for EST

--- config/scheduler_init.js
+++ config/scheduler_init.js
@@ -1,8 +1,25 @@
 const cron = require("node-cron"),
   scheduler = require("./scheduler"),
-  etaReset = require("./eta_reset");
+  etaReset = require("./eta_reset"),
+  moment = require("moment-timezone");
 
 module.exports = function initScheduler() {
   cron.schedule("*/5 * * * *", scheduler);
-  cron.schedule("0 0 * * *", etaReset);
+
+  //TODO: This is a ETA reset for EST. Should make a field for timezone for user
+  // Calculate the UTC hour for midnight EST/EDT
+  const now = moment.tz("America/New_York");
+  const midnightEST = now
+    .clone()
+    .hours(0)
+    .minutes(0)
+    .seconds(0)
+    .milliseconds(0);
+  const midnightUTC = midnightEST.clone().tz("UTC");
+
+  const hourUTC = midnightUTC.hours();
+
+  // Schedule the etaReset job for midnight EST/EDT
+  const scheduleExpression = `0 ${hourUTC} * * *`;
+  cron.schedule(scheduleExpression, etaReset);
 };
