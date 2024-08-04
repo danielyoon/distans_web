de09a65c0ab46b3552defcadcde49e3ff817e501
Daniel Yoon <danielyoonx3@gmail.com>
2024-08-04 03:16:23 -0400

    Testing without Role.Admin on authorize

--- controllers/coupon_controller.js
+++ controllers/coupon_controller.js
@@ -4,12 +4,12 @@ var express = require("express"),
   { ROLE } = require("../components/enums"),
   couponService = require("../services/coupon_service");
 
-router.post("/create-app-coupon", authorize(ROLE.Admin), createAppCoupon);
-router.post("/create-coupon", authorize(), createCoupon);
-router.get("/get-coupons", authorize(), getCoupons);
-router.post("/get-place-coupons", authorize(), getPlaceCoupons);
-router.post("/redeem-coupon", authorize(), redeemCoupon);
-router.post("/delete-coupon", authorize(), deleteCoupon);
+router.post("/create-app-coupon", authorize(), createAppCoupon);
+// router.post("/create-coupon", authorize(), createCoupon);
+// router.get("/get-coupons", authorize(), getCoupons);
+// router.post("/get-place-coupons", authorize(), getPlaceCoupons);
+// router.post("/redeem-coupon", authorize(), redeemCoupon);
+// router.post("/delete-coupon", authorize(), deleteCoupon);
 
 module.exports = router;
 
@@ -26,67 +26,69 @@ function createAppCoupon(req, res, next) {
     .catch(next);
 }
 
-function createCoupon(req, res, next) {
-  couponService
-    .createCoupon(req.auth.id, req.body)
-    .then((result) => {
-      if (result.status === "SUCCESS") {
-        res.status(200).json(result.data);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
-}
+// function createCoupon(req, res, next) {
+//   console.log("Running function");
 
-function getCoupons(req, res, next) {
-  couponService
-    .getCoupons(req.auth.id)
-    .then((result) => {
-      if (result.status === "SUCCESS") {
-        res.status(200).json(result.data);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
-}
+//   couponService
+//     .createCoupon(req.auth.id, req.body)
+//     .then((result) => {
+//       if (result.status === "SUCCESS") {
+//         res.status(200).json(result.data);
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     })
+//     .catch(next);
+// }
 
-function getPlaceCoupons(req, res, next) {
-  couponService
-    .getPlaceCoupons(req.auth.id)
-    .then((result) => {
-      if (result.status === "SUCCESS") {
-        res.status(200).json(result.data);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
-}
+// function getCoupons(req, res, next) {
+//   couponService
+//     .getCoupons(req.auth.id)
+//     .then((result) => {
+//       if (result.status === "SUCCESS") {
+//         res.status(200).json(result.data);
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     })
+//     .catch(next);
+// }
 
-function redeemCoupon(req, res, next) {
-  couponService
-    .redeemCoupon(req.auth.id, req.body)
-    .then((result) => {
-      if (result.status === "SUCCESS") {
-        res.status(200).json(result.data);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
-}
+// function getPlaceCoupons(req, res, next) {
+//   couponService
+//     .getPlaceCoupons(req.auth.id)
+//     .then((result) => {
+//       if (result.status === "SUCCESS") {
+//         res.status(200).json(result.data);
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     })
+//     .catch(next);
+// }
 
-function deleteCoupon(req, res, next) {
-  couponService
-    .deleteCoupon(req.auth.id, req.body)
-    .then((result) => {
-      if (result.status === "SUCCESS") {
-        res.status(200).json(result.data);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
-}
+// function redeemCoupon(req, res, next) {
+//   couponService
+//     .redeemCoupon(req.auth.id, req.body)
+//     .then((result) => {
+//       if (result.status === "SUCCESS") {
+//         res.status(200).json(result.data);
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     })
+//     .catch(next);
+// }
+
+// function deleteCoupon(req, res, next) {
+//   couponService
+//     .deleteCoupon(req.auth.id, req.body)
+//     .then((result) => {
+//       if (result.status === "SUCCESS") {
+//         res.status(200).json(result.data);
+//       } else {
+//         res.sendStatus(404);
+//       }
+//     })
+//     .catch(next);
+// }
