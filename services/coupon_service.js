7d82f1ca66585693b70f3bfcb2ad106346672731
Daniel Yoon <danielyoonx3@gmail.com>
2024-08-07 14:00:21 -0400

    Completed create app coupon function

--- services/coupon_service.js
+++ services/coupon_service.js
@@ -5,9 +5,6 @@ module.exports = {
 };
 
 async function createAppCoupon(id, params) {
-  console.log(id);
-  console.log(params);
-
   let coupon = new db.Coupon({
     id: params.id,
     createdBy: id,
@@ -17,14 +14,6 @@ async function createAppCoupon(id, params) {
     isAppCoupon: true,
   });
 
-  console.log("Coupon:");
-  console.log(coupon);
-
-  try {
-    await coupon.save();
-    return { status: "SUCCESS", data: coupon };
-  } catch (error) {
-    console.error("Error saving coupon:", error);
-    return { status: "ERROR", error: error.message };
-  }
+  await coupon.save();
+  return { status: "SUCCESS", data: coupon };
 }
