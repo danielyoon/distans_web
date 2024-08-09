d3c80c8f37b26e671ad29783d3a0a7900f03a4b0
Daniel Yoon <danielyoonx3@gmail.com>
2024-08-09 02:01:39 -0400

    Added try catch block to createAppCoupon

--- services/coupon_service.js
+++ services/coupon_service.js
@@ -6,21 +6,26 @@ module.exports = {
 };
 
 async function createAppCoupon(id, params) {
-  console.log(params);
+  try {
+    console.log(params);
 
-  let coupon = new db.Coupon({
-    name: params.name,
-    createdBy: id,
-    description: params.description,
-    amount: params.amount,
-    image: "distans",
-    isAppCoupon: true,
-  });
+    let coupon = new db.Coupon({
+      name: params.name,
+      createdBy: id,
+      description: params.description,
+      amount: params.amount,
+      image: "distans",
+      isAppCoupon: true,
+    });
 
-  console.log(coupon);
+    console.log(coupon);
 
-  await coupon.save();
-  return { status: "SUCCESS", data: coupon };
+    await coupon.save();
+    return { status: "SUCCESS", data: coupon };
+  } catch (error) {
+    console.error("Error creating coupon:", error);
+    return { status: "ERROR", message: error.message };
+  }
 }
 
 async function getCoupons(id) {
