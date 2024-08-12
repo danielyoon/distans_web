6be881dd19a71819dee692d11e8a10c4feb6fa1d
Daniel Yoon <danielyoonx3@gmail.com>
2024-08-12 19:47:05 -0400

    Testing createPlace

--- services/map_service.js
+++ services/map_service.js
@@ -8,36 +8,45 @@ module.exports = {
 };
 
 async function createPlace(id, params) {
-  console.log(params);
+  try {
+    console.log(params);
 
-  const place = new db.Place({
-    name: params.name,
-    description: params.description,
-    type: params.type,
-    isPrivate: params.isPrivate,
-    requestedBy: id,
-    location: {
-      type: "Point",
-      coordinates: [params.longitude, params.latitude],
-    },
-  });
+    const place = new db.Place({
+      name: params.name,
+      description: params.description,
+      type: params.type,
+      isPrivate: params.isPrivate,
+      requestedBy: id,
+      location: {
+        type: "Point",
+        coordinates: [params.longitude, params.latitude],
+      },
+    });
 
-  console.log(place);
+    console.log(place);
 
-  await place.save();
+    await place.save();
 
-  if (params.isPrivate) {
-    const user = await db.User.findById(id);
+    if (params.isPrivate) {
+      const user = await db.User.findById(id);
 
-    place.approved = true;
+      if (!user) {
+        throw new Error(`User with id ${id} not found`);
+      }
 
-    user.privatePlaces.push(place._id);
-    await user.save();
+      place.approved = true;
+
+      user.privatePlaces.push(place._id);
+      await user.save();
+    }
+
+    console.log(place);
+
+    return { status: "SUCCESS" };
+  } catch (error) {
+    console.error("Error creating place:", error);
+    return { status: "ERROR" };
   }
-
-  console.log(place);
-
-  return { status: "SUCCESS" };
 }
 
 async function getNearbyPlaces(params) {
