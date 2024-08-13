8ec9db8b9f86b8102abdf8dc6e87ccc7e95afc57
Daniel Yoon <danielyoonx3@gmail.com>
2024-08-12 23:40:11 -0400

    Removed EST from String

--- services/user_service.js
+++ services/user_service.js
@@ -726,7 +726,7 @@ function formatTimeEST(date) {
   hours = hours ? hours : 12; // the hour '0' should be '12'
   const minutesStr = minutes < 10 ? "0" + minutes : minutes;
 
-  const formattedTime = `${hours}:${minutesStr} ${ampm} EST`;
+  const formattedTime = `${hours}:${minutesStr} ${ampm}`;
   return formattedTime;
 }
 
